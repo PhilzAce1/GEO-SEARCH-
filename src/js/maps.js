ed2ceff1e383e59aa3baa151a5490f6694daf809
Philzace1 <unknownumeh@gmail.com>
2019-11-21 21:14:58 +0100

    testing

--- src/js/maps.js
+++ src/js/maps.js
@@ -1,42 +1,64 @@
- function initMap() {
-    // map options
-    var options = {
-        zoom:8, 
-        center:{lat: 42.3601, lng: -71.0589},
-    }
-    var map = new google.maps.Map(document.getElementById('map'), options)
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+function initMap() {
+  var searched;
+  var map;
+  var location = 'Lagos';
+  axios
+    .get('https://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: location,
+        key: 'AIzaSyBYGTJio78JOAqEhbKu8Sj2geI4iYk_7wU'
+      }
+    })
+    .then(res => {
+      searched = {
+        lat: parseFloat(res.data.results[0].geometry.location.lat),
+        lng: parseFloat(res.data.results[0].geometry.location.lng)
+      };
+      //   lat = parseFloat(res.data.results[0].geometry.location.lat);
+      //   lng = parseFloat(res.data.results[0].geometry.location.lat);
 
-
-    google.maps.event.addListener(map, 'click' ,
-    function(event){
-        addMarker({coords:event.latLng})
-        // console.log(event.latLng)
+      console.log(searched);
     })
-    addMarker({
-        coords:{lat: 42.4668,lng: -70.9495},
-        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        // content:'<h1>something</h1>'
-    })
-    // Add Marker 
-    function addMarker(props){
-
-        var marker = new google.maps.Marker({
-         position:props.coords,
-         map:map,
-        //  icon:props.iconImage
-    })
-    if (props.iconImage){
-        marker.setIcon(props.iconImage)
-    }
-    if(props.content){
-        
-    var infoWindow =  new google.maps.InfoWindow({
-        content:props.content
-    })
-    marker.addListener('click', () => {
-        infoWindow.open(map, marker)
-    })
-
-    }
-  }
-}  
+    .catch(err => console.error(err));
+  // map options
+  var options = {
+    zoom: 8,
+    center: searched
+  };
+  map = new google.maps.Map(document.getElementById('map'), options);
+  //   var marker = new google.maps.Marker({
+  //     position: searched,
+  //     // icon: iconBase + 'info-i_maps.png',
+  //     map: map
+  //   });
+  //   google.maps.event.addListener(map, 'click', function(event) {
+  //     addMarker({ coords: event.latLng });
+  //     // console.log(event.latLng)
+  //   });
+  //   addMarker({
+  //     coords: { lat: lat, lng: lng },
+  //     iconImage:
+  //       'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+  //     // content:'<h1>something</h1>'
+  //   });
+  // //   // Add Marker
+  //   function addMarker(props) {
+  //     var marker = new google.maps.Marker({
+  //       position: props.coords,
+  //       map: map
+  //       //  icon:props.iconImage
+  //     });
+  //     if (props.iconImage) {
+  //       marker.setIcon(props.iconImage);
+  //     }
+  //     if (props.content) {
+  //       var infoWindow = new google.maps.InfoWindow({
+  //         content: props.content
+  //       });
+  //       marker.addListener('click', () => {
+  //         infoWindow.open(map, marker);
+  //       });
+  //     }
+  //   }
+}
