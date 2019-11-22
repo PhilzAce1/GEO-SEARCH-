56d72c716d98ed17100aaa585ed767463e2ee37d
Philzace1 <unknownumeh@gmail.com>
2019-11-22 21:23:48 +0100

    Final commit for hosting

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,62 @@
 const gulp = require('gulp');
-const jshint = require('gulp-jshint');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 
+gulp.task('processHTML', done => {
+  done();
+  gulp.src('*.html').pipe(gulp.dest('dist'));
+});
+gulp.task('processIMG', done => {
+  done();
+  gulp.src('images/*.jpg').pipe(gulp.dest('dist/images'));
+});
 
-
-
-gulp.task('processJS', (done) => {
-    done();
-   return gulp.src('src/js/*.js')
-    .pipe(jshint({
-        esversion: 8
-    }))
-    .pipe(jshint.reporter('default'))
-    .pipe(babel({
+gulp.task('processJS', done => {
+  done();
+  gulp
+    .src('*.js')
+    .pipe(
+      babel({
         presets: ['@babel/env']
-    }))
+      })
+    )
     .pipe(uglify())
-    .pipe(gulp.dest('src/dist/js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
-
-gulp.task('babel-polyfill', (done) => {
-    done();
-    return gulp.src('node_modules/babel-polyfill/browser.js')
-        .pipe(gulp.dest('src/dist/node_modules/'));
+gulp.task('babelPolyfill', done => {
+  done();
+  gulp
+    .src('node_modules/babel-polyfill/browser.js')
+    .pipe(gulp.dest('dist/node_modules/babel-polyfill'));
 });
 
-gulp.task('serve',(done) => {
-    done();
-    browserSync.init({
-        server:'./src',
-        port: 8080,
-        ui: {
-            port:8081
-        }
-    });
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: './dist',
+    port: 8080,
+    ui: {
+      port: 8081
+    }
+  });
+  // gulp.watch('*.js', ['processJS']);
+  // gulp.watch('*.html', ['processHTML']);
 
-    gulp.watch(['src/index.html','src/scss/*.scss'],gulp.series(['sass'])).on('change',browserSync.reload);
+  gulp.watch('dist/js/*.js').on('change', browserSync.reload);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
 });
 
-
-gulp.task('default',gulp.series('sass','processJS','babel-polyfill','serve'),(done) => {
-    done();
-});
+gulp.task(
+  'default',
+  gulp.series(
+    'processHTML',
+    'processJS',
+    'processIMG',
+    'babelPolyfill',
+    'browserSync',
+    callback => {
+      callback();
+    }
+  )
+);
